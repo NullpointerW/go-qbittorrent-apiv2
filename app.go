ca2f57592f92c89121d734292f4f9fd2a719c5e7
NullpointerW <825708370@qq.com>
2025-03-21 21:33:10 +0800

    try to fix

--- app.go
+++ app.go
@@ -5,124 +5,155 @@ import (
 	"io"
 )
 
+// I am sure about proxy_type in API >4.5 is string type...
+// doc is outdata for so long,but noone fix it.
+// https://github.com/qbittorrent/qBittorrent/issues/20027#issuecomment-1826858530
 type proxyTyp int
 
+func (p proxyTyp) String() string {
+	switch p {
+	case Http,
+		HttpA:
+		return "HTTP"
+	case Socks5, Socks5A:
+		return "SOCKS5"
+	case Socks4:
+		return "SOCKS4"
+	default:
+		return "None"
+	}
+}
+
 const (
-	Http    proxyTyp = iota + 1 // HTTP proxy without authentication
-	Socks5                      // SOCKS5 proxy without authentication
-	HttpA                       // HTTP proxy with authentication
-	Socks5A                     // SOCKS5 proxy with authentication
-	Socks4                      // SOCKS4 proxy without authentication
+	Nil     proxyTyp = iota
+	Http             // HTTP proxy without authentication
+	Socks5           // SOCKS5 proxy without authentication
+	HttpA            // HTTP proxy with authentication
+	Socks5A          // SOCKS5 proxy with authentication
+	Socks4           // SOCKS4 proxy without authentication
 )
 
 type Config struct {
-	AddTrackers                        string         `json:"add_trackers"`
-	AddTrackersEnabled                 bool           `json:"add_trackers_enabled"`
-	AltDLLimit                         int            `json:"alt_dl_limit"`
-	AltUpLimit                         int            `json:"alt_up_limit"`
-	AlternativeWebuiEnabled            bool           `json:"alternative_webui_enabled"`
-	AlternativeWebuiPath               string         `json:"alternative_webui_path"`
-	AnnounceIP                         string         `json:"announce_ip"`
-	AnnounceToAllTiers                 bool           `json:"announce_to_all_tiers"`
-	AnnounceToAllTrackers              bool           `json:"announce_to_all_trackers"`
-	AnonymousMode                      bool           `json:"anonymous_mode"`
-	AsyncIoThreads                     int            `json:"async_io_threads"`
-	AutoDeleteMode                     int            `json:"auto_delete_mode"`
-	AutoTmmEnabled                     bool           `json:"auto_tmm_enabled"`
-	AutorunEnabled                     bool           `json:"autorun_enabled"`
-	AutorunOnTorrentAddedEnabled       bool           `json:"autorun_on_torrent_added_enabled"`
-	AutorunOnTorrentAddedProgram       string         `json:"autorun_on_torrent_added_program"`
-	AutorunProgram                     string         `json:"autorun_program"`
-	BannedIPS                          string         `json:"banned_IPs"`
-	BittorrentProtocol                 int            `json:"bittorrent_protocol"`
-	BlockPeersOnPrivilegedPorts        bool           `json:"block_peers_on_privileged_ports"`
-	BypassAuthSubnetWhitelist          string         `json:"bypass_auth_subnet_whitelist"`
-	BypassAuthSubnetWhitelistEnabled   bool           `json:"bypass_auth_subnet_whitelist_enabled"`
-	BypassLocalAuth                    bool           `json:"bypass_local_auth"`
-	CategoryChangedTmmEnabled          bool           `json:"category_changed_tmm_enabled"`
-	CheckingMemoryUse                  int            `json:"checking_memory_use"`
-	ConnectionSpeed                    int            `json:"connection_speed"`
-	CurrentInterfaceAddress            string         `json:"current_interface_address"`
-	CurrentNetworkInterface            string         `json:"current_network_interface"`
-	Dht                                bool           `json:"dht"`
-	DiskCache                          int            `json:"disk_cache"`
-	DiskCacheTTL                       int            `json:"disk_cache_ttl"`
-	DiskIoReadMode                     int            `json:"disk_io_read_mode"`
-	DiskIoType                         int            `json:"disk_io_type"`
-	DiskIoWriteMode                    int            `json:"disk_io_write_mode"`
-	DiskQueueSize                      int            `json:"disk_queue_size"`
-	DLLimit                            int            `json:"dl_limit"`
-	DontCountSlowTorrents              bool           `json:"dont_count_slow_torrents"`
-	DyndnsDomain                       string         `json:"dyndns_domain"`
-	DyndnsEnabled                      bool           `json:"dyndns_enabled"`
-	DyndnsPassword                     string         `json:"dyndns_password"`
-	DyndnsService                      int            `json:"dyndns_service"`
-	DyndnsUsername                     string         `json:"dyndns_username"`
-	EmbeddedTrackerPort                int            `json:"embedded_tracker_port"`
-	EmbeddedTrackerPortForwarding      bool           `json:"embedded_tracker_port_forwarding"`
-	EnableCoalesceReadWrite            bool           `json:"enable_coalesce_read_write"`
-	EnableEmbeddedTracker              bool           `json:"enable_embedded_tracker"`
-	EnableMultiConnectionsFromSameIP   bool           `json:"enable_multi_connections_from_same_ip"`
-	EnablePieceExtentAffinity          bool           `json:"enable_piece_extent_affinity"`
-	EnableUploadSuggestions            bool           `json:"enable_upload_suggestions"`
-	Encryption                         int            `json:"encryption"`
-	ExcludedFileNames                  string         `json:"excluded_file_names"`
-	ExcludedFileNamesEnabled           bool           `json:"excluded_file_names_enabled"`
-	ExportDir                          string         `json:"export_dir"`
-	ExportDirFin                       string         `json:"export_dir_fin"`
-	FilePoolSize                       int            `json:"file_pool_size"`
-	HashingThreads                     int            `json:"hashing_threads"`
-	IdnSupportEnabled                  bool           `json:"idn_support_enabled"`
-	IncompleteFilesEXT                 bool           `json:"incomplete_files_ext"`
-	IPFilterEnabled                    bool           `json:"ip_filter_enabled"`
-	IPFilterPath                       string         `json:"ip_filter_path"`
-	IPFilterTrackers                   bool           `json:"ip_filter_trackers"`
-	LimitLANPeers                      bool           `json:"limit_lan_peers"`
-	LimitTCPOverhead                   bool           `json:"limit_tcp_overhead"`
-	LimitUTPRate                       bool           `json:"limit_utp_rate"`
-	ListenPort                         int            `json:"listen_port"`
-	Locale                             string         `json:"locale"`
-	Lsd                                bool           `json:"lsd"`
-	MailNotificationAuthEnabled        bool           `json:"mail_notification_auth_enabled"`
-	MailNotificationEmail              string         `json:"mail_notification_email"`
-	MailNotificationEnabled            bool           `json:"mail_notification_enabled"`
-	MailNotificationPassword           string         `json:"mail_notification_password"`
-	MailNotificationSender             string         `json:"mail_notification_sender"`
-	MailNotificationSMTP               string         `json:"mail_notification_smtp"`
-	MailNotificationSSLEnabled         bool           `json:"mail_notification_ssl_enabled"`
-	MailNotificationUsername           string         `json:"mail_notification_username"`
-	MaxActiveCheckingTorrents          int            `json:"max_active_checking_torrents"`
-	MaxActiveDownloads                 int            `json:"max_active_downloads"`
-	MaxActiveTorrents                  int            `json:"max_active_torrents"`
-	MaxActiveUploads                   int            `json:"max_active_uploads"`
-	MaxConcurrentHTTPAnnounces         int            `json:"max_concurrent_http_announces"`
-	MaxConnec                          int            `json:"max_connec"`
-	MaxConnecPerTorrent                int            `json:"max_connec_per_torrent"`
-	MaxRatio                           int            `json:"max_ratio"`
-	MaxRatioAct                        int            `json:"max_ratio_act"`
-	MaxRatioEnabled                    bool           `json:"max_ratio_enabled"`
-	MaxSeedingTime                     int            `json:"max_seeding_time"`
-	MaxSeedingTimeEnabled              bool           `json:"max_seeding_time_enabled"`
-	MaxUploads                         int            `json:"max_uploads"`
-	MaxUploadsPerTorrent               int            `json:"max_uploads_per_torrent"`
-	MemoryWorkingSetLimit              int            `json:"memory_working_set_limit"`
-	OutgoingPortsMax                   int            `json:"outgoing_ports_max"`
-	OutgoingPortsMin                   int            `json:"outgoing_ports_min"`
-	PeerTos                            int            `json:"peer_tos"`
-	PeerTurnover                       int            `json:"peer_turnover"`
-	PeerTurnoverCutoff                 int            `json:"peer_turnover_cutoff"`
-	PeerTurnoverInterval               int            `json:"peer_turnover_interval"`
-	PerformanceWarning                 bool           `json:"performance_warning"`
-	Pex                                bool           `json:"pex"`
-	PreallocateAll                     bool           `json:"preallocate_all"`
-	ProxyAuthEnabled                   bool           `json:"proxy_auth_enabled"`
-	ProxyHostnameLookup                bool           `json:"proxy_hostname_lookup"`
-	ProxyIP                            string         `json:"proxy_ip"`
-	ProxyPassword                      string         `json:"proxy_password"`
-	ProxyPeerConnections               bool           `json:"proxy_peer_connections"`
-	ProxyPort                          int            `json:"proxy_port"`
-	ProxyTorrentsOnly                  bool           `json:"proxy_torrents_only"`
-	ProxyType                          proxyTyp       `json:"proxy_type"`
+	ConfigWithOutProxyType
+	ProxyType proxyTyp `json:"proxy_type"`
+}
+type ConfigVerUnder461 struct {
+	*Config
+}
+type ConfigVerUper461 struct {
+	*Config
+	ProxyType string `json:"proxy_type"`
+}
+
+type ConfigWithOutProxyType struct {
+	version                          int
+	AddTrackers                      string `json:"add_trackers"`
+	AddTrackersEnabled               bool   `json:"add_trackers_enabled"`
+	AltDLLimit                       int    `json:"alt_dl_limit"`
+	AltUpLimit                       int    `json:"alt_up_limit"`
+	AlternativeWebuiEnabled          bool   `json:"alternative_webui_enabled"`
+	AlternativeWebuiPath             string `json:"alternative_webui_path"`
+	AnnounceIP                       string `json:"announce_ip"`
+	AnnounceToAllTiers               bool   `json:"announce_to_all_tiers"`
+	AnnounceToAllTrackers            bool   `json:"announce_to_all_trackers"`
+	AnonymousMode                    bool   `json:"anonymous_mode"`
+	AsyncIoThreads                   int    `json:"async_io_threads"`
+	AutoDeleteMode                   int    `json:"auto_delete_mode"`
+	AutoTmmEnabled                   bool   `json:"auto_tmm_enabled"`
+	AutorunEnabled                   bool   `json:"autorun_enabled"`
+	AutorunOnTorrentAddedEnabled     bool   `json:"autorun_on_torrent_added_enabled"`
+	AutorunOnTorrentAddedProgram     string `json:"autorun_on_torrent_added_program"`
+	AutorunProgram                   string `json:"autorun_program"`
+	BannedIPS                        string `json:"banned_IPs"`
+	BittorrentProtocol               int    `json:"bittorrent_protocol"`
+	BlockPeersOnPrivilegedPorts      bool   `json:"block_peers_on_privileged_ports"`
+	BypassAuthSubnetWhitelist        string `json:"bypass_auth_subnet_whitelist"`
+	BypassAuthSubnetWhitelistEnabled bool   `json:"bypass_auth_subnet_whitelist_enabled"`
+	BypassLocalAuth                  bool   `json:"bypass_local_auth"`
+	CategoryChangedTmmEnabled        bool   `json:"category_changed_tmm_enabled"`
+	CheckingMemoryUse                int    `json:"checking_memory_use"`
+	ConnectionSpeed                  int    `json:"connection_speed"`
+	CurrentInterfaceAddress          string `json:"current_interface_address"`
+	CurrentNetworkInterface          string `json:"current_network_interface"`
+	Dht                              bool   `json:"dht"`
+	DiskCache                        int    `json:"disk_cache"`
+	DiskCacheTTL                     int    `json:"disk_cache_ttl"`
+	DiskIoReadMode                   int    `json:"disk_io_read_mode"`
+	DiskIoType                       int    `json:"disk_io_type"`
+	DiskIoWriteMode                  int    `json:"disk_io_write_mode"`
+	DiskQueueSize                    int    `json:"disk_queue_size"`
+	DLLimit                          int    `json:"dl_limit"`
+	DontCountSlowTorrents            bool   `json:"dont_count_slow_torrents"`
+	DyndnsDomain                     string `json:"dyndns_domain"`
+	DyndnsEnabled                    bool   `json:"dyndns_enabled"`
+	DyndnsPassword                   string `json:"dyndns_password"`
+	DyndnsService                    int    `json:"dyndns_service"`
+	DyndnsUsername                   string `json:"dyndns_username"`
+	EmbeddedTrackerPort              int    `json:"embedded_tracker_port"`
+	EmbeddedTrackerPortForwarding    bool   `json:"embedded_tracker_port_forwarding"`
+	EnableCoalesceReadWrite          bool   `json:"enable_coalesce_read_write"`
+	EnableEmbeddedTracker            bool   `json:"enable_embedded_tracker"`
+	EnableMultiConnectionsFromSameIP bool   `json:"enable_multi_connections_from_same_ip"`
+	EnablePieceExtentAffinity        bool   `json:"enable_piece_extent_affinity"`
+	EnableUploadSuggestions          bool   `json:"enable_upload_suggestions"`
+	Encryption                       int    `json:"encryption"`
+	ExcludedFileNames                string `json:"excluded_file_names"`
+	ExcludedFileNamesEnabled         bool   `json:"excluded_file_names_enabled"`
+	ExportDir                        string `json:"export_dir"`
+	ExportDirFin                     string `json:"export_dir_fin"`
+	FilePoolSize                     int    `json:"file_pool_size"`
+	HashingThreads                   int    `json:"hashing_threads"`
+	IdnSupportEnabled                bool   `json:"idn_support_enabled"`
+	IncompleteFilesEXT               bool   `json:"incomplete_files_ext"`
+	IPFilterEnabled                  bool   `json:"ip_filter_enabled"`
+	IPFilterPath                     string `json:"ip_filter_path"`
+	IPFilterTrackers                 bool   `json:"ip_filter_trackers"`
+	LimitLANPeers                    bool   `json:"limit_lan_peers"`
+	LimitTCPOverhead                 bool   `json:"limit_tcp_overhead"`
+	LimitUTPRate                     bool   `json:"limit_utp_rate"`
+	ListenPort                       int    `json:"listen_port"`
+	Locale                           string `json:"locale"`
+	Lsd                              bool   `json:"lsd"`
+	MailNotificationAuthEnabled      bool   `json:"mail_notification_auth_enabled"`
+	MailNotificationEmail            string `json:"mail_notification_email"`
+	MailNotificationEnabled          bool   `json:"mail_notification_enabled"`
+	MailNotificationPassword         string `json:"mail_notification_password"`
+	MailNotificationSender           string `json:"mail_notification_sender"`
+	MailNotificationSMTP             string `json:"mail_notification_smtp"`
+	MailNotificationSSLEnabled       bool   `json:"mail_notification_ssl_enabled"`
+	MailNotificationUsername         string `json:"mail_notification_username"`
+	MaxActiveCheckingTorrents        int    `json:"max_active_checking_torrents"`
+	MaxActiveDownloads               int    `json:"max_active_downloads"`
+	MaxActiveTorrents                int    `json:"max_active_torrents"`
+	MaxActiveUploads                 int    `json:"max_active_uploads"`
+	MaxConcurrentHTTPAnnounces       int    `json:"max_concurrent_http_announces"`
+	MaxConnec                        int    `json:"max_connec"`
+	MaxConnecPerTorrent              int    `json:"max_connec_per_torrent"`
+	MaxRatio                         int    `json:"max_ratio"`
+	MaxRatioAct                      int    `json:"max_ratio_act"`
+	MaxRatioEnabled                  bool   `json:"max_ratio_enabled"`
+	MaxSeedingTime                   int    `json:"max_seeding_time"`
+	MaxSeedingTimeEnabled            bool   `json:"max_seeding_time_enabled"`
+	MaxUploads                       int    `json:"max_uploads"`
+	MaxUploadsPerTorrent             int    `json:"max_uploads_per_torrent"`
+	MemoryWorkingSetLimit            int    `json:"memory_working_set_limit"`
+	OutgoingPortsMax                 int    `json:"outgoing_ports_max"`
+	OutgoingPortsMin                 int    `json:"outgoing_ports_min"`
+	PeerTos                          int    `json:"peer_tos"`
+	PeerTurnover                     int    `json:"peer_turnover"`
+	PeerTurnoverCutoff               int    `json:"peer_turnover_cutoff"`
+	PeerTurnoverInterval             int    `json:"peer_turnover_interval"`
+	PerformanceWarning               bool   `json:"performance_warning"`
+	Pex                              bool   `json:"pex"`
+	PreallocateAll                   bool   `json:"preallocate_all"`
+	ProxyAuthEnabled                 bool   `json:"proxy_auth_enabled"`
+	ProxyHostnameLookup              bool   `json:"proxy_hostname_lookup"`
+	ProxyIP                          string `json:"proxy_ip"`
+	ProxyPassword                    string `json:"proxy_password"`
+	ProxyPeerConnections             bool   `json:"proxy_peer_connections"`
+	ProxyPort                        int    `json:"proxy_port"`
+	ProxyTorrentsOnly                bool   `json:"proxy_torrents_only"`
+
 	ProxyUsername                      string         `json:"proxy_username"`
 	QueueingEnabled                    bool           `json:"queueing_enabled"`
 	RandomPort                         bool           `json:"random_port"`
